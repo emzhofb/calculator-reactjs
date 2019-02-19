54ea377b7b07cd704ce53a69a9883832771a802f
emzhofb <ikhdamuhammad@gmail.com>
2019-02-19 15:28:56 +0700

    kurang sedikit kelar

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super ()
+    this.state = {
+      inputan: '',
+      number: 0
+    }
+  }
+
+  _handleLoop = () => {
+    const inputanKu = this.state.inputan
+    let numTemp = []
+    let stringNum = ''
+    let lastNum = ''
+
+    for (let i = 0; i < inputanKu.length; i++) {
+      if (inputanKu[i] === '1' || inputanKu[i] === '2' || inputanKu[i] === '3' || inputanKu[i] === '4' || inputanKu[i] === '5' || inputanKu[i] === '6' || inputanKu[i] === '7' || inputanKu[i] === '8' || inputanKu[i] === '9' || inputanKu[i] === '0') {
+        stringNum += inputanKu[i]
+      } else {
+        numTemp.push(stringNum)
+        stringNum = ''
+        numTemp.push(inputanKu[i])
+      }
+    }
+    for (let i = 0; i < inputanKu.length; i++) {
+      if (inputanKu[i] === '*' || inputanKu[i] === '/' || inputanKu[i] === '+' || inputanKu[i] === '-') {
+        lastNum = '';
+      } else {
+        lastNum += inputanKu[i]
+      }
+    }
+    numTemp.push(lastNum)
+
+    let result = Number(numTemp[0])
+    for (let i = 1; i < numTemp.length; i++) {
+      if (numTemp[i] === '*') {
+        let j = i+1;
+        result = result * Number(numTemp[j])
+      }
+      if (numTemp[i] === '/') {
+        let j = i+1;
+        result = result / Number(numTemp[j])
+      }
+      if (numTemp[i] === '+') {
+        let j = i+1;
+        result = result + Number(numTemp[j])
+      }
+      if (numTemp[i] === '-') {
+        let j = i+1;
+        result = result - Number(numTemp[j])
+      }
+    }
+
+    this.setState({
+      number: result
+    })
+  }
+
+  _handleChange = (element) => {
+    this.setState({[element.target.name]: element.target.value})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <input
+          value = {this.state.inputan}
+          onInput = {this._handleLoop}
+          onChange = {this._handleChange}
+          name = "inputan"
+        />
+        <p>{this.state.number}</p>
       </div>
     );
   }
